f1d465d693bed844cf815bd20a30081410ed973c
Subh130 <subhchaudhary541@gmail.com>
2025-05-10 17:27:13 +0545

    L02

---
+++ L02 understanding variable.py
@@ -0,0 +1,16 @@
+# Creating different variables
+x = 5
+y = "Jhon"
+z = 7.3
+print(x)
+print(y)
+print(z)
+ 
+word="codingal"
+print(word)
+ 
+ # user input
+name = input("enter your name= ")
+print("Hello"+ name, "welcome to codingal" )
+
+age=int(input("how old are you? "))
---
+++ L02 welcome to the world of coding.py
@@ -0,0 +1,14 @@
+#Print a sentence
+print("Welcome to the world of programming!")
+
+#print a number
+print(4)
+
+#Next line after print
+print("hello python \n")
+
+#Pass multiple arguments for prinitng
+print("hello",5)
+
+#End argument of print statement
+print("welcome to ",end = "+")
