77f1c06e408ad7e7df7a05d7f07bcdb56bfaf98f
Subh130 <subhchaudhary541@gmail.com>
2025-05-18 13:19:29 +0545

    L05

---
+++ L05 summer times.py
@@ -0,0 +1,7 @@
+temp = int(input("enter the temperature in degree celcius: "))
+
+if temp >= 21:
+    print("wear light clothes its hot outside")
+
+else:
+    print("wear a jacket and pullover its cold outside")
