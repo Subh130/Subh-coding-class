6f86df045883f0fd7509b4fda15ba75ee055a5b8
Subh130 <subhchaudhary541@gmail.com>
2025-05-24 13:41:07 +0545

    L07

---
+++ L07 check.py
@@ -0,0 +1,127 @@
+
+char = input("enter a single character: ")
+
+if char == "a":
+    print("the ascii code is 97 ")
+elif char == "b":
+    print("the ascii code is 98 ")
+elif char == "c":
+    print("the ascii code is 99 ")
+elif char == "d":
+    print("the ascii code is 100 ")
+elif char == "e":
+    print("the ascii code is 101 ")
+elif char == "f":
+    print("the ascii code is 102 ")
+elif char == "g":
+    print("the ascii code is 103 ")
+elif char == "h":
+    print("the ascii code is 104 ")
+elif char == "i":
+    print("the ascii code is 105 ")
+elif char == "j":
+    print("the ascii code is 106 ")
+elif char == "k":
+    print("the ascii code is 107 ")
+elif char == "l":
+    print("the ascii code is 108 ")
+elif char == "m":
+    print("the ascii code is 109")
+elif char == "n":
+    print("the ascii code is 110 ")
+elif char == "o":
+    print("the ascii code is 111 ")
+elif char == "p":
+    print("the ascii code is 112 ")
+elif char == "q":
+    print("the ascii code is 113 ")
+elif char == "r":
+    print("the ascii code is 114 ")
+elif char == "s":
+    print("the ascii code is 115 ")
+elif char == "t":
+    print("the ascii code is 116 ")
+elif char == "u":
+    print("the ascii code is 117 ")
+elif char == "v":
+    print("the ascii code is 118 ")
+elif char == "w":
+    print("the ascii code is 119 ")
+elif char == "x":
+    print("the ascii code is 120 ")
+elif char == "y":
+    print("the ascii code is 121 ")
+elif char == "z":
+    print("the ascii code is 122 ")
+elif char == "A":
+    print("the ascii code is 65 ")
+elif char == "B":
+    print("the ascii code is 66 ")
+elif char == "C":
+    print("the ascii code is 67 ")
+elif char == "D":
+    print("the ascii code is 68 ")
+elif char == "E":
+    print("the ascii code is 69 ")
+elif char == "F":
+    print("the ascii code is 70 ")
+elif char == "G":
+    print("the ascii code is 71 ")
+elif char == "H":
+    print("the ascii code is 72 ")
+elif char == "I":
+    print("the ascii code is 73 ")
+elif char == "J":
+    print("the ascii code is 74 ")
+elif char == "K":
+    print("the ascii code is 75 ")
+elif char == "L":
+    print("the ascii code is 76 ")
+elif char == "M":
+    print("the ascii code is 77")
+elif char == "N":
+    print("the ascii code is 78 ")
+elif char == "O":
+    print("the ascii code is 79 ")
+elif char == "P":
+    print("the ascii code is 80 ")
+elif char == "Q":
+    print("the ascii code is 81 ")
+elif char == "R":
+    print("the ascii code is 82 ")
+elif char == "S":
+    print("the ascii code is 83 ")
+elif char == "T":
+    print("the ascii code is 84 ")
+elif char == "U":
+    print("the ascii code is 85 ")
+elif char == "V":
+    print("the ascii code is 86 ")
+elif char == "W":
+    print("the ascii code is 87 ")
+elif char == "X":
+    print("the ascii code is 88 ")
+elif char == "Y":
+    print("the ascii code is 89 ")
+elif char == "Z":
+    print("the ascii code is 90 ")
+elif char == "0" :
+    print("the ascii code is 48 ")
+elif char == "1" :
+    print("the ascii code is 49 ")
+elif char == "2":
+    print("the ascii code is 50 ")
+elif char == "3":
+    print("the ascii code is 51 ")
+elif char == "4":
+    print("the ascii code is 52 ")
+elif char == "5":
+    print("the ascii code is 53 ")
+elif char == "6":
+    print("the ascii code is 54")
+elif char == "7":
+    print("the ascii code is 55 ")
+elif char == "8":
+    print("the ascii code is 56 ")
+elif char == "9":
+    print("the ascii code is 57 ")
