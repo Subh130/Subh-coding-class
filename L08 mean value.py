bf8a42b26ea7ae297c386b9d6889be109477a596
Subh130 <subhchaudhary541@gmail.com>
2025-05-24 16:25:40 +0545

    L08

---
+++ L08 mean value.py
@@ -0,0 +1,16 @@
+mean1 = 38 
+wrong_number = 36 
+correct_number = 56
+total_number = 40
+
+#Sum of 40 nubmers 
+sum = mean1*total_number
+print("The sum of 40 number: ", sum)
+
+#Correct sum of these nubmers 
+num2 = sum - ((wrong_number)- (correct_number))
+print("sum-((Worng_number)- (Correct_number)): ", num2 )
+
+#The correct mean 
+mean2 = num2/ total_number
+print(f"the correct mean: {mean2}")
