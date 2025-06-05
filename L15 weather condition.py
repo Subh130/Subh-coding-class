648ab0d76784475b81f13213a5fc9175fdf0adf3
Subh130 <subhchaudhary541@gmail.com>
2025-06-05 21:05:20 +0545

    L15

---
+++ L15 weather condition.py
@@ -0,0 +1,8 @@
+def weather_condtition(): # Declaring the function
+    print("Weather is plesant in",x)
+    print("weather is not plesant in",y)
+
+x = "autumn"
+y = "winter"
+
+weather_condtition()# call the function
