d10d958e7b2b83f69e7d9f3114a9f6c738e7c34c
Subh130 <subhchaudhary541@gmail.com>
2025-06-07 16:01:45 +0545

    L17

---
+++ L17 break.py
@@ -0,0 +1,8 @@
+word = input("Enter a word: ")
+
+for i in word:
+    if (i == "a"):
+        print("a is found")
+        break # Stop the repetition process and get out of the loop
+    else:
+        print("a not found")
