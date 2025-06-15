68044d765496393e99c0544e1ea314781c9e8df2
Subh130 <subhchaudhary541@gmail.com>
2025-06-15 16:17:51 +0545

    L24

---
+++ L24 set intersection.py
@@ -0,0 +1,12 @@
+setx = {"green", "blue"}
+sety = {"blue", "yellow"}
+
+print("original set elements: ")
+print(setx)
+print(sety)
+print()
+
+print("intersection of two said sets:")
+setz = setx.intersection(sety)
+
+print(setz)
