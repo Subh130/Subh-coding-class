1890238f842be9fb0de2fe14100dfacb9d45b961
Subh130 <subhchaudhary541@gmail.com>
2025-06-20 19:31:43 +0545

    L27

---
+++ L27 student class.py
@@ -0,0 +1,8 @@
+#Create a class
+class student:
+    grade = 10 #variable or feature
+    print("Hi i am an student of grade", grade)
+
+#create an object
+subh = student() #object 1 
+jack = student() #object 2 
