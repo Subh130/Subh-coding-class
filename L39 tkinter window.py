863fe7ac19917c485d26721018d3f609130aa633
Subh130 <subhchaudhary541@gmail.com>
2025-07-26 19:15:57 +0545

    L39

---
+++ L39 tkinter window.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+#create a window
+window= Tk()
+
+#set window geometry
+window.title("First window")
+window.geometry("400x300") #window size: width = 400 , height  = 300
+
+window.mainloop()
